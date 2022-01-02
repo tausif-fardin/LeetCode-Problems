10e3b32d7797bc3eedc25eeb6bd38a2170b28aa0
Tausif <tausiffardin4@gmail.com>
2022-01-02 15:11:12 +0600

    commit

---
+++ 1_Two_Sum.py
@@ -0,0 +1,22 @@
+
+# Brute force solution
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            sum = nums[i]+nums[j]
+            if sum == target:
+                return [i, j]
+
+# O(n) better solution
+
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    complementMap = dict()
+    for i in range(len(nums)):
+        num = nums[i]
+        complement = target - num
+
+        if num in complementMap:
+            return [complementMap[num], i]
+        else:
+            complementMap[complement] = i
