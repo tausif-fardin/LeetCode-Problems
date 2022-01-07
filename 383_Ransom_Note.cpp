6326bf99f5f2436970e3359c3a8e70d5921eb081
Tausif <tausiffardin4@gmail.com>
2022-01-07 23:57:28 +0600

    Commit

--- 383_Ransom_Note.cpp
+++ 383_Ransom_Note.cpp
@@ -1,17 +1,21 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-bool canConstruct(string ransomNote, string magazine) {
-    vector<int> magazineHash(26,0);
-        
-    for(int i=0;magazine[i]!='\0';i++){
-        magazineHash[magazine[i]-'a']+=1;
+bool canConstruct(string ransomNote, string magazine)
+{
+    vector<int> magazineHash(26, 0);
+
+    for (int i = 0; magazine[i] != '\0'; i++)
+    {
+        magazineHash[magazine[i] - 'a'] += 1;
     }
-        
-    for(int i=0;ransomNote[i]!='\0';i++){
-        if(magazineHash[ransomNote[i]-'a']==0)
+
+    for (int i = 0; ransomNote[i] != '\0'; i++)
+    {
+        if (magazineHash[ransomNote[i] - 'a'] == 0)
             return false;
-        magazineHash[ransomNote[i]-'a']-=1;
+        magazineHash[ransomNote[i] - 'a'] -= 1;
     }
     return true;
 }
