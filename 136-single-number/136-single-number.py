d29235199fb11749e865062003b2cdb28a149fbe
Tausif Fardin <58599676+tausif-fardin@users.noreply.github.com>
2022-10-05 23:51:20 +0600

    Time: 310 ms (31.78%), Space: 16.8 MB (19.01%) - LeetHub

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = {}
+        for num in nums:
+            dic[num] = dic.get(num, 0) + 1
+        for key, value in dic.items():
+            if value == 1:
+                return key
+        
