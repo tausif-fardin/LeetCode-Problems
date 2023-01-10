a78172619a1b6f485ec219e220c89a60b37d2716
Tausif Fardin <58599676+tausif-fardin@users.noreply.github.com>
2023-01-10 11:31:20 +0600

    Time: 34 ms (82.11%), Space: 13.9 MB (52.29%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        #first two fibonacci numbers
+        f = [0,1]
+        
+        for i in range(2, n+1):
+            f.append(f[i-1] + f[i-2])
+        return f[n]
+        
